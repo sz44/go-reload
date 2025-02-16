bb1078bd47d43bc7e1433b0766a23f068de88228
sz44 <semion44@gmail.com>
2025-02-16 11:57:24 -0800

    removed string builder

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 // cli tool goreload program.go
 // step 1: given program run it
 // ../oop/adapter.go
+// go run main.go ../oop/adapter.go
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintln(os.Stderr, "usage: goreload <program>")
@@ -25,8 +25,6 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	var b strings.Builder
-	b.Write(out)
 
-	fmt.Println(b.String())
+	fmt.Printf("%s", out)
 }
